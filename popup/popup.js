a65378fb4de6f07a7b09c1b9ff72d0fecb1a91b9
Ujjwal-here <ujjwalsbx@gmail.com>
2023-09-29 15:48:54 +0530

    Add listener for Generation

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,25 @@
 console.log("Popup Runs")
+
+
+const generateButton=document.getElementsByClassName("generate")
+const stopButton=document.getElementsByClassName("save")
+const downloadButton=document.getElementsByClassName("download")
+
+generateButton[0].addEventListener("click",function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {message:"Generate",tabUrl:tabs[0].url});
+    });
+    return true
+})
+
+stopButton[0].addEventListener("click",function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {message:"Stop",tabUrl:tabs[0].url});
+    });
+    return true
+})
+
+
+downloadButton[0].addEventListener("click", function (){
+    console.log("Download PDF")
+})
