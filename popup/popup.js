88b028fd0c95234ca3f8b4f2a0782a8ef16053cd
Ujjwal-here <ujjwalsbx@gmail.com>
2023-10-02 10:53:12 +0530

    Added Event Listeners for Caption

--- popup/popup.js
+++ popup/popup.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const loginDiv = document.getElementById("login")
     const popupDiv = document.getElementById("popup")
 
+    const generateButton = document.getElementsByClassName("generate")
+    const stopButton = document.getElementsByClassName("save")
+
     const toSignUp = document.getElementById("toSignUp")
     const toLogin = document.getElementById("toLogin")
 
@@ -79,6 +82,21 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 
+    generateButton[0].addEventListener("click", function () {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {message: "Generate", tabUrl: tabs[0].url});
+        });
+        return true
+    })
+
+    stopButton[0].addEventListener("click", function () {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {message: "Stop", tabUrl: tabs[0].url});
+        });
+        return true
+    })
+
+
     chrome.storage.sync.get("token", function (res) {
         if (res.token) {
             popupDiv.style.display = "block"
@@ -95,25 +113,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 })
 
-// const generateButton=document.getElementsByClassName("generate")
-// const stopButton=document.getElementsByClassName("save")
-// const downloadButton=document.getElementsByClassName("download")
-//
-// generateButton[0].addEventListener("click",function () {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//         chrome.tabs.sendMessage(tabs[0].id, {message:"Generate",tabUrl:tabs[0].url});
-//     });
-//     return true
-// })
-//
-// stopButton[0].addEventListener("click",function () {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//         chrome.tabs.sendMessage(tabs[0].id, {message:"Stop",tabUrl:tabs[0].url});
-//     });
-//     return true
-// })
 
 
-//downloadButton[0].addEventListener("click", function (){
-//    console.log("Download PDF")
-//})
